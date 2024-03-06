e006bb4dd10e5e9db8e5e9e7c60499ac2b6ca3ad
Gabigol09 <123607615+Gabigol09@users.noreply.github.com>
2024-03-06 20:16:33 -0300

    Update Calculator.jsx

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,30 +1,35 @@
+// Importando os componentes necessários do React e o estilo do Material-UI
 import React, { useState } from "react";
 import "./Calculator.css";
 import Container from "@mui/material/Container";
 import { Box } from "@mui/system";
 
+// Componente funcional para a Calculadora
 export default function Calculator() {
+// Variáveis de estado para o número atual, número antigo e operador
   const [num, setNum] = useState(0);
   const [oldnum, setOldNum] = useState(0);
   const [operator, setOperator] = useState();
 
+// Função para lidar com a entrada de números
   function inputNum(e) {
     var input = e.target.value;
+// Atualiza o número atual com base na entrada
     if (num === 0) {
       setNum(input);
     } else {
       setNum(num + input);
     }
   }
-
+// Função para limpar o número atual
   function clear() {
     setNum(0);
   }
-
+// Função para calcular a porcentagem do número atual
   function porcentage() {
     setNum(num / 100);
   }
-
+// Função para alterar o sinal do número atual
   function changeSign() {
     if (num > 0) {
       setNum(-num);
@@ -32,14 +37,16 @@ export default function Calculator() {
       setNum(Math.abs(num));
     }
   }
-
+// Função para lidar com a entrada de operadores
   function operatorHandler(e) {
     var operatorInput = e.target.value;
+        // Salva o número atual como o número antigo, define o operador e reinicia o número atual
     setOperator(operatorInput);
     setOldNum(num);
     setNum(0);
   }
 
+// Função para realizar o cálculo com base no operador selecionado
   function calculate() {
     if (operator === "/") {
       setNum(parseFloat(oldnum) / parseFloat(num));
@@ -55,6 +62,7 @@ export default function Calculator() {
     }
   }
 
+// Estrutura JSX para renderizar o componente da Calculadora
   return (
     <div>
       <Box m={5} />
